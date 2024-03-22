8d5008d48ec42ccb9cc2e910619467bc2d786aea
Abhishek Shekhawat <85601866+Abhishek-S-2001@users.noreply.github.com>
2024-03-22 20:16:19 +0530

    Created Add Post Card

--- src/Components/Post/post.js
+++ src/Components/Post/post.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
 import './post.css'
-import { Grid, Typography, TextareaAutosize, InputBase } from '@mui/material';
+import { Typography, TextField, Switch, Button, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Post = () => {
     const [showCard, setShowCard] = useState(false);
+    
     const [title, setTitle] = useState('');
     const [thought, setThought] = useState('');
-    const [tags, setTags] = useState('');
+    const [selectedtag, setTag] = useState('');
+    const [Public, setPublic] = useState('');
+
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const tags = [ "#Technology", "#Cooking", "#Idea", "#Travel", "#Photography", "#Fashion",
+        "#Fitness", "#Art", "#Music", "#Books", "#Foodie", "#DIY", "#Nature", "#Gaming",
+        "#Health", "#Motivation", "#Inspiration", "#Quotes", "#Selfie", "#ThrowbackThursday" ];
+      
 
     const handleThoughtChange = (event) => {
         setThought(event.target.value);
@@ -18,13 +28,15 @@ const Post = () => {
         element.style.height = element.scrollHeight + 'px';
       };
 
-    const handlePostButtonClick = () => {
-        // Add logic to handle posting the title, thought, and tags
+    const handleSubmit = () => {
         console.log('Title:', title);
         console.log('Thought:', thought);
-        console.log('Tags:', tags);
-        // You can send the data to an API, store in state, etc.
+        console.log('Tags:', selectedtag);
+        console.log('Public: ', Public);
 
+
+        // Post Uploaded successful
+        setSuccessMessage('Post Uploaded');
         // Close the card after posting
         setShowCard(false);
     };
@@ -34,43 +46,52 @@ const Post = () => {
             <button className='button__newpost' onClick={() => setShowCard(true)}>
                 Have new thought!!! Post IT</button>
         ) : (
-    <div className="card-overlay">
-        <Grid className="post__card" container spacing={1}>
-            <Grid item xs={12}>
-                <Typography className='card__label' variant="h6">
-                <span style={{ color: '#FF0000' }}>A</span>dd Ne
-                <span style={{ transform: 'rotate(-90deg)', color: '#FF0000', display: 'inline-block' }}>E </span>
-                <span style={{ color: '#FF0000' }}> T</span>hou
-                <span style={{ transform: 'rotate(-90deg)', color: '#FF0000', display: 'inline-block' }}>G</span>
-                h<span style={{ color: '#FF0000' }}>T</span>
-                </Typography>
-            </Grid>
-            <Grid item xs= {12}>
-                <InputBase className='card__title'
-                    placeholder="Add Title" value={title}
-                    onChange={(e) => setTitle(e.target.value)} />
-            </Grid>
-            <Grid item xs = {12}>
-            <TextareaAutosize
-                className="card__thought"
-                placeholder="Enter Thought"
-                value={thought}
-                onChange={handleThoughtChange}
-              />
-            </Grid>
-            <Grid item xm= {6}>
-                <select className='card__tags' value={tags}
-                onChange={(e) => setTags(e.target.value)}>
-                <option value="">Select Tags</option>
-                <option value="tag1">Tag 1</option>
-                <option value="tag2">Tag 2</option>
-                {/* Add more options as needed */}
-                </select>
-            </Grid>
-            <Grid item xm={6}>
-                <button className='post__button' onClick={handlePostButtonClick}>Post</button>
-            </Grid>
-        </Grid>
+    <div className="card-overlay" >
+        <div className="newpost__card" container spacing={1}>
+            <div className='newpostcontent__container'>
+                <div style={{display: 'flex', justifyContent:'center', alignItems:'center', margin:'20px'}}>
+                    <Typography className='postcard__label' variant="h6">
+                        <span style={{ color: '#FF0000' }}>A</span>dd Ne
+                        <span style={{ transform: 'rotate(-90deg)', color: '#FF0000', display: 'inline-block' }}>E </span>
+                        <span style={{ color: '#FF0000' }}> T</span>hou
+                        <span style={{ transform: 'rotate(-90deg)', color: '#FF0000', display: 'inline-block' }}>G</span>
+                        h<span style={{ color: '#FF0000' }}>T</span>
+                    </Typography>
+                    <CloseIcon style={{marginLeft: '80px', color:'Grey'}} onClick={() => setShowCard(false)} />
+                </div>
+                <form className='newpost__form' onSubmit={handleSubmit}>
+                    <TextField className='newpost__textfield' label="Title" 
+                    value={title}  onChange={(e) => setTitle(e.target.value)}
+                    required />
+                    <TextField className = 'newpost__textfield' id="outlined-multiline-static" 
+                        label="Thought" multiline rows={6}
+                        value={thought}
+                        onChange={handleThoughtChange} required />
+                    <div style={{display:'flex', alignItems:'center'}}>
+                        <FormControl style={{width:'150px'}}>
+                        <InputLabel id="tag-select-label">Tag</InputLabel>
+                        <Select labelId="tag-select-label" id="demo-simple-select"
+                            value={selectedtag} label="Tag"
+                            onChange={(e) => setTag(e.target.value)} placeholder='Tag'>
+                            {tags.map((tag,index) => (
+                                <MenuItem key={index} value={tag}>{tag}</MenuItem>
+                            ))};
+                        </Select>
+                        </FormControl>
+                        <span style={{ margin: '20px' }}>Public</span>
+                        <Switch defaultChecked value={Public} onChange={(e) => setPublic(e.target.value)}/>
+                    </div>
+                    <Button type="submit" variant="contained" color="primary">
+                    Post
+                    </Button>
+                </form>
+                {successMessage && (
+                    <div className="success" onAnimationEnd={() => setSuccessMessage('')}>
+                    Successfully Registered
+                    </div>
+                )}
+            </div>
+        </div>
     </div>
         )}
     </div>
